5ef80e1d62dfaaa6a67001d79c5911418e35f9b1
xingfei <xingfei0831@gmail.com>
2013-05-06 16:27:30 +0800

    slight modify

--- src/com/github/xingfei/octopus/ServerPipelineFactory.java
+++ src/com/github/xingfei/octopus/ServerPipelineFactory.java
@@ -1,5 +1,7 @@
 package com.github.xingfei.octopus;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.jboss.netty.bootstrap.ClientBootstrap;
@@ -11,20 +13,32 @@ import org.jboss.netty.handler.codec.frame.DelimiterBasedFrameDecoder;
 import org.jboss.netty.handler.codec.frame.Delimiters;
 import org.jboss.netty.handler.codec.string.StringDecoder;
 
-import com.github.xingfei.octopus.dht.DistributeHashTable;
+import com.github.xingfei.octopus.dht.ConsistentHash;
 import com.github.xingfei.octopus.pool.ConnectionPool;
 import com.github.xingfei.octopus.redis.RedisRequestDecoder;
 
+/**
+ * The serverside ChannelPipelineFactory implementation
+ * @author xingfei
+ *
+ */
 public class ServerPipelineFactory implements ChannelPipelineFactory {
 
 	private ConcurrentHashMap<String, ConnectionPool> pools;
-	private DistributeHashTable<BackendServer> dht;
+	private ConsistentHash dht;
 	private ClientBootstrap bootstrap;
 
 	public ServerPipelineFactory(ServerConfig config,
 			ChannelFactory clientChannelFactory) {
 		this.pools = new ConcurrentHashMap<>();
-		this.dht = new DistributeHashTable<>(config.getServers(), config.getVirtualNodes());
+		List<BackendServer> backends = new ArrayList<>();
+		for(String serverName: config.servers.keySet()) {
+			ServerConfig.Server server = config.servers.get(serverName);
+			String[] t = server.address.split(":");
+			BackendServer backend = new BackendServer(t[0], Integer.parseInt(t[1]), server.weight);
+			backends.add(backend);
+		}
+		this.dht = new ConsistentHash(backends, config.virtualNode);
 		this.bootstrap = new ClientBootstrap(clientChannelFactory);
 		this.bootstrap.setPipelineFactory(new ClientPipelineFactory());
 	}
